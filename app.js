4770ae04418ed596a2b6b80dc7db8a5985fe7f5c
matheusAznavour <matheus.lins896@gmail.com>
2024-12-12 16:49:27 -0300

    adding a graph engine

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const ctx = document.getElementById('myChart');
+
 const tBody = document.getElementById("tbodyTable");
 let avaibleTimeInput = document.getElementById("avaibleTimeInput").value
 let currentLevelInput = document.getElementById("currentLevelInput").value
@@ -11,40 +13,40 @@ const clTw = 478000 //Cealing twenth level
 let globalOutput = ""
 
 
-function xpAcquired(level){
-	let arithmetic = ((level - 20) * clXp) + clTw
-	return arithmetic
+function xpAcquired(level) {
+  let arithmetic = ((level - 20) * clXp) + clTw
+  return arithmetic
 }
 
-function minutesNeeded(level){
-	let arithmetic = (xpAcquired(level) / 5600)  * 20
-	return parseInt(arithmetic)
+function minutesNeeded(level) {
+  let arithmetic = (xpAcquired(level) / 5600) * 20
+  return parseInt(arithmetic)
 }
 
-function xpNeeded(){
-    console.log(`
+function xpNeeded() {
+  console.log(`
         CURRENT LEVEL: ${currentLevelInput}
         DESIREBLE INPUT: ${desirebleLevelInput}
         `)
-    let arithmetic = xpAcquired(parseInt(desirebleLevelInput)) - xpAcquired(parseInt(currentLevelInput))
-    return arithmetic
+  let arithmetic = xpAcquired(parseInt(desirebleLevelInput)) - xpAcquired(parseInt(currentLevelInput))
+  return arithmetic
 }
 
-function hoursNeeded(){
-	let arithmetic = (xpNeeded()/280)/60
-	return parseInt(arithmetic)
+function hoursNeeded() {
+  let arithmetic = (xpNeeded() / 280) / 60
+  return parseInt(arithmetic)
 }
 
-function daysNeeded(){
-    let arithmetic = hoursNeeded()/avaibleTimeInput
-	return parseInt(arithmetic)
+function daysNeeded() {
+  let arithmetic = hoursNeeded() / avaibleTimeInput
+  return parseInt(arithmetic)
 }
 
 //  ^
 //  | 8==D
 
-function chart(level, i){
-    tBody.innerHTML += `
+function chart(level, i) {
+  tBody.innerHTML += `
                   <tr>
 
                 <td>${level}</td>
@@ -55,17 +57,17 @@ function chart(level, i){
               `
 }
 
-for(let a = 1; a < 100; a++){
-    chart(a + 19, a)
+for (let a = 1; a < 100; a++) {
+  chart(a + 19, a)
 }
 
-function simulator(){
-    avaibleTimeInput = document.getElementById("avaibleTimeInput").value
-    currentLevelInput = document.getElementById("currentLevelInput").value
-    desirebleLevelInput = document.getElementById("desirebleLevelInput").value
-    console.log(daysNeeded())
+function simulator() {
+  avaibleTimeInput = document.getElementById("avaibleTimeInput").value
+  currentLevelInput = document.getElementById("currentLevelInput").value
+  desirebleLevelInput = document.getElementById("desirebleLevelInput").value
+  console.log(daysNeeded())
 
-    resultTable.innerHTML = `
+  resultTable.innerHTML = `
                     <table class="table table-striped" id="resultTabled">
                     <thead>
                       <tr>
@@ -94,8 +96,56 @@ function simulator(){
                     </tbody>
                   </table>
     `
-
+let newLabels = [currentLevelInput, currentLevelInput+1, currentLevelInput+2, currentLevelInput+3, currentLevelInput+4]
+let newData = [hoursNeeded(), hoursNeeded()+3, hoursNeeded()+4, hoursNeeded()+3, hoursNeeded()+4]
+graphicChart(mychart, newLabels, )
 }
 
+function graphicChart(chart, newLabels, newData) {
+  chart.data.labels = newLabels; // Atualiza os rótulos
+  chart.data.datasets[0].data = newData; // Atualiza os dados
+  chart.update(); // Atualiza o gráfico visualmente
+}
+
+const mychart =  new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ["20", '21', '22', '23', '24', '25'],
+    datasets: [{
+      label: 'level per hours',
+      data: [28, 32, 35, 39, 42, 46],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
 
 
+/*
+function graphicChart(d1) {
+  const mychart =  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [d1, '21', '22', '23', '24', '25'],
+      datasets: [{
+        label: 'level per hours',
+        data: [28, 32, 35, 39, 42, 46],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
+  */
